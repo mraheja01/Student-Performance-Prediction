217145d61c069598bc3a7aa465c9e58fe2dfd033
mraheja01 <142112660+mraheja01@users.noreply.github.com>
2025-05-10 12:21:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mlproject",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="madhav",
+    author_email="madhavraheja01@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
